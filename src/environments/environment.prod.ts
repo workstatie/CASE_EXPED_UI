c11610b6c6612bb46d5be1054b3a830e5700fd87
Workstation <workstatie@outlook.com>
2020-06-10 19:49:53 +0300

    added changes to new req

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/',
-  robotUrl: 'https://workstatie.ddns.net/robot/',
+  apiUrl: 'https://192.168.1.10/api/',
+  robotUrl: 'https://192.168.1.10/robot/',
   refreshRate: 2000,
   oktaClient: '0oac15y5vPpeoqWWQ4x6',
-  oktaRedirect :'https://workstatie.ddns.net/login'
+  oktaRedirect :'https://192.168.1.10/login'
 };
